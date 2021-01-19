2457885e3994e249520b02b80306caddd4c6084b
Emily Reinhart <53956110+emily-reinhart@users.noreply.github.com>
2021-01-19 17:40:13 -0600

    fix router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Fragment } from 'react';
 import Nav from './components/Nav';
-import { Switch, Route } from 'react-router-dom';
+
 import Main from './components/Main';
 import Resume from './components/Resume';
+import { Switch, Route, } from 'react-router'
 
 function App () {
 	return (
 		<Fragment>
 			<Nav></Nav>
 			<Switch>
-				<Route exact path="/" component={Main} />
-				<Route path="/resume" component={Resume} />
+			<Route exact path="/" component={Main} />
+ 			<Route path="/resume" component={Resume} />
 			</Switch>
-		</Fragment>
-		
+		</Fragment>		
 	);
 }
 
